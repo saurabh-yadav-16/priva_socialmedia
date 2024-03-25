23f35afba894593f4912dde5b930951b3d99a1dd
saurabh-yadav-16 <saurabhyadav1610@gmail.com>
2024-03-25 18:07:00 +0530

    Update dependencies

--- lib/features/auth/repositery/auth_repositry.dart
+++ lib/features/auth/repositery/auth_repositry.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: use_build_context_synchronously, duplicate_ignore
-
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
